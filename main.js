3370f8f4d51a53626165a61e94b5d863157969e1
Isaac Aguiar <isaacaguiar_@hotmail.com>
2022-03-30 23:00:29 -0300

    menu and about

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-window.alert('Hello World!')
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+const links = document.querySelectorAll('nav ul li a')
+
+// para cada elemento dentro da const, remover ou add a class show
+// formas de criar uma funcion: const nameFunction = function() {}; function() {} e arrow function: () => {}
+
+for (const element of toggle) {
+    element.addEventListener('click', () => {
+        nav.classList.toggle('show')
+    } )
+}
+
+// ao clicar em algum dos links, remover a class show
+
+for ( const element of links) {
+    element.addEventListener('click', () => {
+        nav.classList.remove('show')
+    })
+}
+
+// parei o video em 1:33
